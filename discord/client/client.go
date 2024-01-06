f2b77fd7a01648e53e090a0a250dbe2c82153cf7
Michael Fraser <mj.fraser99@icloud.com>
2024-01-06 20:00:20 +0000

    added missing client addition

--- discord/client/client.go
+++ discord/client/client.go
@@ -3,14 +3,17 @@ package client
 import (
 	"github.com/MichaelFraser99/discord-application-sdk/discord/config"
 	"github.com/MichaelFraser99/discord-application-sdk/services/application_commands"
+	"github.com/MichaelFraser99/discord-application-sdk/services/applications"
 )
 
 type Client struct {
 	ApplicationCommand application_commands.ApplicationCommandsAPI
+	Application        applications.ApplicationAPI
 }
 
 func NewClient(cfg *config.Config) *Client {
 	return &Client{
 		ApplicationCommand: application_commands.New(cfg),
+		Application:        applications.New(cfg),
 	}
 }
